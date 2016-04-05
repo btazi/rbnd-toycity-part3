fe1729cb602ed0c0407e9c296cded6f219ae13b6
Badr Tazi <tasibadr@gmail.com>
2016-04-05 15:22:12 +0100

    added duplicated error to the product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,24 @@
 class Product
+	attr_accessor :title, :price, :stock
 
 	@@products = []
 
 	def initialize(options={})
-		@@products << self
+		@title = options[:title]
+		add_to_products(options)
 	end
 
 	def self.all
 		@@products
+	end
+
+	private
+
+	def add_to_products(options)
+		if @@products.any?{|product| product.title == options[:title]}
+			raise DuplicateProductError, "'#{options[:title]}' already exists."
+		else
+			@@products << self
+		end
 	end
 end
